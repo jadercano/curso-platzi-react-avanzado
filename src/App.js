ffd22faf652d101afb66d46795f9a467e054288a
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-28 16:30:53 -0500

    Added following Components: PhotoCard, ListOfPhotoCards, Logo. Added animations.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { ListOfPhotoCards } from "./ListOfPhotoCards";
+import { Logo } from "./Logo";
 
 const App = () => {
   return (
     <>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </>
   );
 };
