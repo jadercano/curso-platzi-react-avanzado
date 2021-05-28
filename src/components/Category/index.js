e231bca06c880e3a1963add8c63e418b418eba05
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-28 14:09:53 -0500

    Added Global Styles. Added List of Categories Components packages.

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -6,5 +6,6 @@ const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpg";
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = "?" }) => (
   <Anchor href={path}>
     <Image src={cover} />
+    {emoji}
   </Anchor>
 );
