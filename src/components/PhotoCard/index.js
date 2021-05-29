55cb17d0c5adde5c7031bdfbe314ca5d24fad382
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-29 07:28:07 -0500

    Fixed ListOfPhotoCards to show only PhotoCard when it is in the viewport.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,40 @@
-import React from "react";
-import { Button, Img, ImgWrapper } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import { Article, Button, Img, ImgWrapper } from "./styles";
 import { MdFavoriteBorder } from "react-icons/md";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, setShow] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        console.log("Yes!");
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(ref.current);
+  }, [ref]);
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt="" />
-        </ImgWrapper>
-      </a>
-      <Button type="button">
-        <MdFavoriteBorder size="32px" /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="" />
+            </ImgWrapper>
+          </a>
+          <Button type="button">
+            <MdFavoriteBorder size="32px" /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   );
 };
