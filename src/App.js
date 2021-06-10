4a63e38a4ea7d02ae488d936ec3547df0111af4d
Jader Cano <jader.cano@contractors.accruent.com>
2021-06-10 17:02:50 -0500

    Added NotFound Component and Fixed Routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { Logo } from "./components/Logo";
 import { Home } from "./pages/Home";
@@ -8,9 +8,12 @@ import { NavBar } from "./components/NavBar";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
-import Context from "./Context";
+import { Context } from "./Context";
+import { NotFound } from "./pages/NotFound";
 
 const App = () => {
+  const { isAuth } = useContext(Context);
+
   return (
     <>
       <BrowserRouter>
@@ -20,21 +23,15 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/pet/:id" component={Home} />
           <Route exact path="/detail/:detailId" component={Detail} />
-          <Context.Consumer>
-            {({ isAuth }) =>
-              isAuth ? (
-                <>
-                  <Route exact path="/favs" component={Favs} />
-                  <Route exact path="/user" component={User} />
-                </>
-              ) : (
-                <>
-                  <Route exact path="/favs" component={NotRegisteredUser} />
-                  <Route exact path="/user" component={NotRegisteredUser} />
-                </>
-              )
-            }
-          </Context.Consumer>
+          {!isAuth && (
+            <Route exact path="/login" component={NotRegisteredUser} />
+          )}
+          {!isAuth && <Redirect from="/favs" to="/login" />}
+          {!isAuth && <Redirect from="/user" to="/login" />}
+          {isAuth && <Redirect from="/login" to="/" />}
+          <Route exact path="/favs" component={Favs} />
+          <Route exact path="/user" component={User} />
+          <Route component={NotFound} />
         </Switch>
         <NavBar />
       </BrowserRouter>
